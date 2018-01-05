20c5ff9f7474a1ebf5c406c816d3fee08d825782
0xEAB <desisma@heidel.beer>
2018-01-05 02:15:00 +0100

    Add package modules for the library part

---
+++ source/midigamepad/lib/package.d
@@ -0,0 +1,9 @@
+module midigamepad.lib;
+
+public
+{
+    import midigamepad.lib.translation;
+    import midigamepad.lib.keyboard;
+    import midigamepad.lib.midi;
+    import midigamepad.lib.util;
+}
---
+++ source/midigamepad/lib/translation/package.d
@@ -0,0 +1,8 @@
+module midigamepad.lib.translation;
+
+public
+{
+    import midigamepad.lib.translation.ioprocessor;
+    import midigamepad.lib.translation.mapping;
+    import midigamepad.lib.translation.mappingsfile;
+}

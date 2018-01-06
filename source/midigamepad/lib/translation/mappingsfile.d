97d4071a74ffb9d406b60fd2628ac8017408b128
0xEAB <desisma@heidel.beer>
2018-01-06 21:11:46 +0100

    Improve onNote mappings parser
    - 'extended' field is no longer required
    - alphabetic keys can now be specified by their char equivalent (case insensitive)
        e.g. b, B, e, E
    - most parsing errors lead now to a catchable MappingsParserException

--- source/midigamepad/lib/translation/mappingsfile.d
+++ source/midigamepad/lib/translation/mappingsfile.d
@@ -17,9 +17,12 @@
  +/
 module midigamepad.lib.translation.mappingsfile;
 
-import std.conv : to;
+import std.ascii : isAlpha;
+import std.conv : ConvException, to;
+import std.exception;
 import std.file : readText;
 import std.json;
+import std.string;
 import std.stdio : write, writeln;
 
 import midigamepad.lib.keyboard;
@@ -43,20 +46,147 @@ MappingsCollection parse(string json)
 {
     MappingsCollection output = MappingsCollection();
 
-    JSONValue root = parseJSON(json, JSONOptions.escapeNonAsciiChars);
-    JSONValue map = root.object["map"];
+    immutable JSONValue root = parseJSON(json, JSONOptions.escapeNonAsciiChars);
+    immutable JSONValue map = root.object["map"];
 
     // onNote mappings
-    JSONValue onNote = map.object["onNote"];
-    output.noteOnOff = new NoteOnOffMapping[onNote.array.length];
-    size_t i = 0;
+    immutable JSONValue onNote = map.object["onNote"];
+    output.noteOnOff = parseNoteOnOffMappings(onNote);
 
-    foreach (JSONValue m; onNote.array)
+    return output;
+}
+
+/++
+    Parses the specified NoteOnOffMappings
+ +/
+NoteOnOffMapping[] parseNoteOnOffMappings(JSONValue noteOnOffRoot)
+{
+    auto ar = noteOnOffRoot.array;
+    NoteOnOffMapping[] output = new NoteOnOffMapping[ar.length];
+
+    foreach (size_t i, JSONValue m; ar)
     {
-        output.noteOnOff[i++] = NoteOnOffMapping(
-                m["vkey"].str.hexOrDecToNumber!uint.toScancode.to!ushort,
-                (m["extended"].type == JSON_TYPE.TRUE), m["note"].str.hexOrDecToNumber!byte);
+        uint vKey = void;
+
+        // Ensure that the required elements exist
+        enforce(("vkey" in m), new MappingsParserException("Missing 'vkey' definition.", m));
+        enforce(("note" in m), new MappingsParserException("Missing 'note' definition.", m));
+
+        immutable string vKeyStr = m["vkey"].str;
+        immutable string noteStr = m["note"].str;
+
+        // Try to parse the vKey
+        if (!vKeyStr.tryParseVKEY(vKey))
+            throw new MappingsParserException("Cannot parse invalid vKey: `" ~ vKeyStr ~ "`", m);
+
+        immutable ushort scancode = vKey.toScancode.to!ushort;
+        immutable bool isExtended = vKey.hasExtendedScancode;
+
+        try
+        {
+            immutable byte note = noteStr.hexOrDecToNumber!byte;
+            output[i] = NoteOnOffMapping(scancode, isExtended, note);
+        }
+        catch (ConvException ex)
+        {
+            throw new MappingsParserException(
+                    "Cannot parse invalid MIDI note number: `" ~ noteStr ~ "`", m, ex);
+        }
     }
 
     return output;
+}
+
+/++
+    Tries to parse the passed vKey string and converts it into a scan
+
+    Params:
+        input   = input vKey string
+        vKey    = contains the vKey
+
+    Returns:
+        true = success
+ +/
+bool tryParseVKEY(string input, out uint vKey) nothrow pure @safe
+{
+    // Check whether vKey is a number (hex or dec)
+    if ((input.startsWith("0x")) ? input[2 .. $].isNumeric : input.isNumeric)
+    {
+        try
+        {
+            vKey = input.hexOrDecToNumber!uint;
+            return true;
+        }
+        catch (Exception)
+        {
+            // Conversion failure
+            return false;
+        }
+    }
+
+    // Check whether vKey is a single char
+    else if ((input.length == 1) && input[0].isAlpha)
+    {
+        vKey = uint(input[0].toUpper);
+        return true;
+    }
+
+    // Not parseable
+    return false;
+}
+
+@safe unittest
+{
+    uint output;
+
+    assert("0x12".tryParseVKEY(output));
+    assert(output == 0x12);
+
+    assert("31".tryParseVKEY(output));
+    assert(output == 31);
+
+    assert("A".tryParseVKEY(output));
+    assert(output == 'A');
+
+    assert("b".tryParseVKEY(output));
+    assert(output == 'B');
+
+    assert(!"".tryParseVKEY(output));
+    assert(!"yy".tryParseVKEY(output));
+    assert(!"DD".tryParseVKEY(output));
+    assert(!"→".tryParseVKEY(output));
+    assert(!"♠".tryParseVKEY(output));
+    assert(!".".tryParseVKEY(output));
+}
+
+/++
+    This is exception is thrown when the parser fails to parse a mapping.
+
+    Check the .faulyMapping property to see which one caused the problem.
+ +/
+class MappingsParserException : Exception
+{
+@nogc nothrow pure @safe:
+
+    private
+    {
+        JSONValue _faultyMapping;
+    }
+
+    /++
+        The faulty mapping that could not be parsed
+     +/
+    public @property JSONValue faultyMapping() const
+    {
+        return this._faultyMapping;
+    }
+
+    /++
+        ctor
+     +/
+    public this(string msg, JSONValue faultyMapping, Throwable next = null)
+    {
+        super(msg, next);
+        this._faultyMapping = faultyMapping;
+    }
 }
